cfb641c0a80e21f1a636c2448a8ac8abc8dfff5c
Arch0125 <dasarchisman25@gmail.com>
2022-04-23 08:35:11 +0530

    Update Image upload link

--- src/components/Upload.jsx
+++ src/components/Upload.jsx
@@ -9,8 +9,9 @@ function Upload() {
             <h3>Upload NFT to IPFS</h3>
             <p>MetaMint supports uploading a complete folder full of images</p>
             <a target="_blank" href='https://y6j2b.csb.app/' ><button className='button' >Select Folder</button></a>
-            <a target="_blank" href='https://y6j2b.csb.app/' ><button className='button' >Select Image</button></a>
+            <a target="_blank" href='https://65kpl.csb.app/' ><button className='button' >Select Image</button></a>
             <a target="_blank" href='/Mint' ><button className='button' >Manage NFT Subscriptions</button></a>
+            
 
         </div>
     </div>  
